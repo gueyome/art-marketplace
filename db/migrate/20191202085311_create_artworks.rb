70f3f2f7936b6d50af24930855143347edff576d
jeremyFRANCOIS <jeremy.francois.esg@gmail.com>
2019-12-02 10:20:17 +0100

    table + controller + model ok

---
+++ db/migrate/20191202085311_create_artworks.rb
@@ -0,0 +1,14 @@
+class CreateArtworks < ActiveRecord::Migration[5.2]
+  def change
+    create_table :artworks do |t|
+      t.string :name
+      t.text :description
+      t.integer :price
+      t.integer :stock, :default => 1
+      t.boolean :creator, :default => false
+      t.belongs_to :category, index: true
+      t.references :artist, index: true
+      t.timestamps
+    end
+  end
+end
