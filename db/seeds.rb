825f594a05ebb86aee45bcefaf01763bca9ce7fa
QnYosa <dimitriyoula@gmail.com>
2019-12-02 16:46:32 +0100

    seed done pr

--- db/seeds.rb
+++ db/seeds.rb
@@ -21,12 +21,12 @@ Testimonial.destroy_all
 
 10.times do |index|
   u = User.create(username: Faker::JapaneseMedia::OnePiece.character, first_name: Faker::Name.first_name, last_name: Faker::Name.first_name, email: "#{Faker::Name.first_name}.#{Faker::Name.last_name}@yopmail.com", password: "xaxaxa")
-  a = Artwork.create(name: Faker::Music::Opera.verdi, description: Faker::Lorem.sentence(word_count: 28, supplemental: true, random_words_to_add: 29), price: Faker::Number.decimal(l_digits: 2), stock: Faker::Number.between(from: 1, to: 15), creator: Faker::Boolean.boolean(true_ratio: 0.2), category_id: Faker::Number.within(range: 1..10), artist_id: Faker::Number.within(range: 1..10) )
+  a = Artwork.create(name: Faker::Music::Opera.verdi, description: Faker::Lorem.sentence(word_count: 15, supplemental: true, random_words_to_add: 16), price: Faker::Number.decimal(l_digits: 2), stock: Faker::Number.between(from: 1, to: 15), creator: Faker::Boolean.boolean(true_ratio: 0.2), category_id: Faker::Number.within(range: 1..10), user_id: Faker::Number.within(range: 1..10) )
   c = Category.create(name: Faker::IndustrySegments.industry)
-  cr = Cart.create(customer_id: Faker::Number.between(from: 1, to: 10))
-  crd = CartDetail.create(artwork_id: Faker::Number.between(from: 1, to: 15), quantity: Faker::Number.between(from: 1, to: 3), cart_id: Faker::Number.between(from: 1, to: 10))
-  o = Order.create(customer_id: Faker::Number.between(from: 1, to: 10), date: Faker::Date.in_date_period)
-  od = OrderDetail.create(artwork_id: Faker::Number.between(from: 1, to: 10), quantity: Faker::Number.between(from: 1, to: 3), order_id: Faker::Number.between(from: 1, to: 10))
+  cr = Cart.create(user_id: Faker::Number.between(from: 1, to: 10))
+  # crd = CartDetail.create(artwork_id: Faker::Number.between(from: 1, to: 15), quantity: Faker::Number.between(from: 1, to: 3), cart_id: Faker::Number.between(from: 1, to: 10))
+  o = Order.create(user_id: Faker::Number.between(from: 1, to: 10), date: Faker::Date.in_date_period)
+  # od = OrderDetail.create(artwork_id: Faker::Number.between(from: 1, to: 10), quantity: Faker::Number.between(from: 1, to: 3), order_id: Faker::Number.between(from: 1, to: 10))
 end
 
 
