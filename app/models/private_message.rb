1d961af055b3480a4d24b3bbafb28b15c9f8e98f
guillaume <gueyome@hotmail.fr>
2019-12-02 10:05:37 +0100

    model-controller-private-message

---
+++ app/models/private_message.rb
@@ -0,0 +1,2 @@
+class PrivateMessage < ApplicationRecord
+end
--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,6 @@
 Rails.application.routes.draw do
+  get 'private_messages/index'
+  get 'private_messages/show'
+  get 'private_messages/new'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
