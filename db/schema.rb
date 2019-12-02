864c44e0bb6d0623c6e1c11ee76b144045acdb4c
jeremyFRANCOIS <jeremy.francois.esg@gmail.com>
2019-12-02 12:44:55 +0100

    artwork-category done

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2019_12_02_113435) do
+ActiveRecord::Schema.define(version: 2019_12_02_114044) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -23,6 +23,8 @@ ActiveRecord::Schema.define(version: 2019_12_02_113435) do
     t.boolean "creator", default: false
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.bigint "category_id"
+    t.index ["category_id"], name: "index_artworks_on_category_id"
   end
 
   create_table "cart_details", force: :cascade do |t|
@@ -102,6 +104,7 @@ ActiveRecord::Schema.define(version: 2019_12_02_113435) do
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
   end
 
+  add_foreign_key "artworks", "categories"
   add_foreign_key "cart_details", "artworks"
   add_foreign_key "cart_details", "carts"
   add_foreign_key "carts", "users"
