c65ebca469c9346720f10077c1722b18e59dd22a
jeremyFRANCOIS <jeremy.francois.esg@gmail.com>
2019-12-04 00:11:49 +0100

    add artwork to cart + check the stock done

--- app/controllers/cart_details_controller.rb
+++ app/controllers/cart_details_controller.rb
@@ -13,20 +13,25 @@ class CartDetailsController < ApplicationController
     @cart = current_user.cart
     @artwork = Artwork.find(params[:artwork_id])
     @quantity_chosen = params[:quantity].to_i
-    if @cart.artworks.include?(@artwork)
-        @artwork_line = @cart.cart_details.where(artwork_id: @artwork.id).first
-        @artwork_line.update(quantity: @artwork_line.quantity + @quantity_chosen)
+    if @quantity_chosen <= @artwork.stock
+      if @cart.artworks.include?(@artwork)
+          @artwork_line = @cart.cart_details.where(artwork_id: @artwork.id).first
+          @artwork_line.update(quantity: @artwork_line.quantity + @quantity_chosen)
+          @artwork.update(stock: @artwork.stock - @quantity_chosen)
+      else
+        @cart_detail_line = CartDetail.new
+        @cart_detail_line.cart_id = @cart.id 
+        @cart_detail_line.artwork_id = @artwork.id 
+        @cart_detail_line.quantity = @quantity_chosen
+        @cart_detail_line.save
         @artwork.update(stock: @artwork.stock - @quantity_chosen)
+      end
+      flash[:success] = "Ajouté au panier."
+      redirect_to root_path
     else
-      @cart_detail_line = CartDetail.new
-      @cart_detail_line.cart_id = @cart.id 
-      @cart_detail_line.artwork_id = @artwork.id 
-      @cart_detail_line.quantity = @quantity_chosen
-      @cart_detail_line.save
-      @artwork.update(stock: @artwork.stock - @quantity_chosen)
+      flash[:warning] = "Quantité trop grande."
+      redirect_to artwork_path(@artwork.id)
     end
-
-    redirect_to root_path
   end
 
   def edit
