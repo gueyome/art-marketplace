bbe38ac88dbfbb4204870550583414a9e82fd47a
QnYosa <dimitriyoula@gmail.com>
2019-12-03 16:02:31 +0100

    seed final

--- db/seeds.rb
+++ db/seeds.rb
@@ -7,37 +7,27 @@ require 'faker'
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
 
-
-User.destroy_all
-Artwork.destroy_all
-Category.destroy_all
-Cart.destroy_all
 CartDetail.destroy_all
-Order.destroy_all
 OrderDetail.destroy_all
+Cart.destroy_all
+Order.destroy_all
 Contact.destroy_all
 PrivateMessage.destroy_all
 Testimonial.destroy_all
+User.destroy_all
+Artwork.destroy_all
+Category.destroy_all
 
-10.times do |index|
-  u = User.create(username: Faker::JapaneseMedia::OnePiece.character, first_name: Faker::Name.first_name, last_name: Faker::Name.first_name, email: "#{Faker::Name.first_name}.#{Faker::Name.last_name}@yopmail.com", password: "xaxaxa")
-  a = Artwork.create(name: Faker::Music::Opera.verdi, description: Faker::Lorem.sentence(word_count: 15, supplemental: true, random_words_to_add: 16), price: Faker::Number.decimal(l_digits: 2), stock: Faker::Number.between(from: 1, to: 15), creator: Faker::Boolean.boolean(true_ratio: 0.2), category_id: Faker::Number.within(range: 1..10), user_id: Faker::Number.within(range: 1..10) )
-  c = Category.create(name: Faker::IndustrySegments.industry)
-  cr = Cart.create(user_id: Faker::Number.between(from: 1, to: 10))
-  # crd = CartDetail.create(artwork_id: Faker::Number.between(from: 1, to: 15), quantity: Faker::Number.between(from: 1, to: 3), cart_id: Faker::Number.between(from: 1, to: 10))
-  o = Order.create(user_id: Faker::Number.between(from: 1, to: 10), date: Faker::Date.in_date_period)
-  # od = OrderDetail.create(artwork_id: Faker::Number.between(from: 1, to: 10), quantity: Faker::Number.between(from: 1, to: 3), order_id: Faker::Number.between(from: 1, to: 10))
-end
+3.times do |index|   
+    u = User.create(username: "user#{index}", first_name: "name#{index}", last_name: index, email: "#{index}@yopmail.com", password: "xaxaxa")
+    c = Category.create(name: index)
+    a = Artwork.create(name: index, description: index, price: 17, creator: index, category_id: c.id, user_id: u.id )
+    ctc = Contact.create(address: index, latitude: index, description: index, longitude: index, phone: index, user_id: u.id)
+    cr = Cart.create(user_id: u.id)
+    o = Order.create(user_id: u.id, date: Time.now)
+    pm = PrivateMessage.create(sender_id: u.id, receiver_id: User.all.first.id, content:index)
+    t = Testimonial.create(artist_id: User.all.first.id, customer_id: u.id, content: index)
+    crd = CartDetail.create(artwork_id: a.id, cart_id: cr.id)
+    od = OrderDetail.create(artwork_id: a.id, order_id: o.id)
 
-
-10.times do |testimonial|
-    ctc = Contact.create(address: Faker::Address.full_address, latitude: Faker::Address.latitude, description: Faker::Lorem.paragraphs(number: 1, supplemental: true), longitude: Faker::Address.longitude, phone: Faker::PhoneNumber.cell_phone_with_country_code, user_id: Faker::Number.between(from: 1, to: 10))
-end 
-
-100.times do |private|
-  pm = PrivateMessage.create(sender_id: Faker::Number.between(from: 1, to: 10), receiver_id: Faker::Number.between(from: 1, to: 10), content: Faker::Quote.matz)
-end 
-
-30.times do |testimonial|
-  t = Testimonial.create(artist_id: Faker::Number.between(from: 1, to: 10), customer_id: Faker::Number.between(from: 1, to: 10), content: Faker::Lorem.paragraphs(number: 1))
 end 
