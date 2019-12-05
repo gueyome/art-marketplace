8ab45e3906778153d8e0061cbcf0072572812656
guillaume <gueyome@hotmail.fr>
2019-12-05 17:53:33 +0100

    user page show and edit ok

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -14,9 +14,17 @@ class UsersController < ApplicationController
   end
 
   def edit
+    @user = User.find(params[:id])
   end
 
   def update
+    @user = User.find(params[:id])
+    puts "*******************************"
+    puts params
+    @user.update(first_name: params[:first_name], last_name: params[:last_name], username: params[:username])
+    @user.contact.update(address: params[:address], description: params[:description], phone: params[:phone])
+    flash[:success] = "Your profile have been modified"
+    redirect_to edit_user_path(@user.id)
   end
 
   def destroy
