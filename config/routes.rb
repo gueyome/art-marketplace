b025d88327e41893540ea09ac897f3b8308d4b9d
QnYosa <dimitriyoula@gmail.com>
2019-12-06 14:21:35 +0100

    Active Storage works in development

--- config/routes.rb
+++ config/routes.rb
@@ -15,4 +15,14 @@ Rails.application.routes.draw do
   root 'pages#index'
   resources :charges
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+
+  Rails.application.routes.draw do
+    resources :users, only: [:show]
+  end
+
+  Rails.application.routes.draw do
+    resources :users, only: [:show] do
+      resources :avatars, only: [:create]
+    end
+  end
 end
