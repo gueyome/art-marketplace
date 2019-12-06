adfac40e98e10030f608a387906793e37aa33b00
guillaume <gueyome@hotmail.fr>
2019-12-06 20:40:30 +0100

    some fix

--- app/controllers/orders_controller.rb
+++ app/controllers/orders_controller.rb
@@ -1,14 +1,9 @@
 class OrdersController < ApplicationController
   skip_before_action :authenticate_user!, only: [:index, :show]
   before_action :create_cart_for_current_user
-<<<<<<< HEAD
   layout "artist_application", :only => [:index]
-
-=======
   before_action :create_contact_for_current_user
-  layout "no_navbar", :only => [:index]
   
->>>>>>> development
   def index
     @orders=Order.where(user_id: current_user.id)
   end
