f7c52a424d81a2e777749e2b7681660ff4cbebb5
jeremyFRANCOIS <jeremy.francois.esg@gmail.com>
2019-12-07 21:44:42 +0100

    search button on the home page done + resize form on artwork show and search show

--- config/routes.rb
+++ config/routes.rb
@@ -13,6 +13,7 @@ Rails.application.routes.draw do
   resources :private_messages
   resources :testimonials 
   resources :search
+  resources :results
   root 'pages#index'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 
