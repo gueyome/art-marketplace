19eba83301e615b5755e8228279ddb683d1090ce
jeremyFRANCOIS <jeremy.francois.esg@gmail.com>
2019-12-11 15:42:56 +0100

    dashboard ok

--- app/controllers/statistic/statistic_controller.rb
+++ app/controllers/statistic/statistic_controller.rb
@@ -1,6 +1,14 @@
 class Statistic::StatisticController < ApplicationController
   def index
-    
+    @artworks = Artwork.all
+    @cart_details = CartDetail.all
+    @carts = Cart.all
+    @categories = Category.all
+    @contacts = Contact.all
+    @order_details = OrderDetail.all
+    @orders = Order.all
+    @private_messages = PrivateMessage.all
+    @testimonials = Testimonial.all
+    @users = User.all
   end
-
 end
