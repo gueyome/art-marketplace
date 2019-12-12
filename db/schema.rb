aa0dd9891c6b8dc75ff1ed27de8ba5ba12aafe7d
QnYosa <dimitriyoula@gmail.com>
2019-12-12 01:43:05 +0100

    migration done

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2019_12_06_182215) do
+ActiveRecord::Schema.define(version: 2019_12_12_002159) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -71,6 +71,7 @@ ActiveRecord::Schema.define(version: 2019_12_06_182215) do
     t.string "name"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.string "url_image"
   end
 
   create_table "contacts", force: :cascade do |t|
