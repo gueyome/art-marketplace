a13f7aa3ec1ef87e307d48b5933291b22efdb92e
QnYosa <dimitriyoula@gmail.com>
2019-12-15 22:26:46 +0100

    methods  name changed

--- app/controllers/cart_details_controller.rb
+++ app/controllers/cart_details_controller.rb
@@ -17,7 +17,7 @@ class CartDetailsController < ApplicationController
     @cart = current_user.cart
     @artwork = Artwork.find(params[:artwork_id])
     if params[:quantity].to_i <= @artwork.stock
-      is_artwork_included?(@cart, @artwork)
+      quantities_updator(@cart, @artwork)
       flash[:success] = "Added to the cart."
       redirect_to user_cart_path(@current_user.id, current_user.cart.id)
     else
@@ -46,19 +46,20 @@ class CartDetailsController < ApplicationController
       redirect_to user_cart_path(@current_user.id, current_user.cart.id)
     end
   end
-  
+
   private
-  def is_artwork_included?(the_cart, the_artwork)
+  def quantities_updator(the_cart, the_artwork)
     if the_cart.artworks.include?(the_artwork)
-        @artwork_line = the_cart.cart_details.where(artwork_id: the_artwork.id).first
-        @artwork_line.update(quantity: @artwork_line.quantity + params[:quantity].to_i)
-        the_artwork.update(stock: the_artwork.stock - params[:quantity].to_i)
+      @artwork_line = the_cart.cart_details.where(artwork_id: the_artwork.id).first
+      @artwork_line.update(quantity: @artwork_line.quantity + params[:quantity].to_i)
+      the_artwork.update(stock: the_artwork.stock - params[:quantity].to_i)
     else
       @cart_detail_line = CartDetail.new(cart_id: the_cart.id, artwork_id: the_artwork.id, quantity: params[:quantity].to_i)
       @cart_detail_line.save
       the_artwork.update(stock: the_artwork.stock - params[:quantity].to_i)
     end
   end 
+  
   def current_line
     @line = CartDetail.find(params[:id])
   end
